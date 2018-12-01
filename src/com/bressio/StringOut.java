9e5c7425b9d2b74ce6f12a4d70f048f023f39e14
Breno Sousa <bress.io@outlook.com>
2018-12-01 15:17:22 -0200

    Aprimorar a legibilidade da I/O

--- src/com/bressio/StringOut.java
+++ src/com/bressio/StringOut.java
@@ -4,17 +4,42 @@ import static com.bressio.StringFormat.repeat;
 
 public final class StringOut {
 
+    private enum Color {
+        RESET("\u001B[0m"), GREEN("\u001B[32m"), RED("\u001B[31m"), BLUE("\u001B[34m");
+
+        private Color(String color) {
+            this.color = color;
+        }
+        private String color;
+
+        public String getColor() {
+            return color;
+        }
+    }
+
     private StringOut() {}
 
     public static void printInputDialog(String str) {
-        System.out.println(repeat("-", str.length()) + '\n' + str + '\n' + repeat("-", str.length()));
+        System.out.print(Color.GREEN.getColor() +
+                repeat("-", str.length()) + '\n' + str + '\n' +
+                repeat("-", str.length()) +
+                '\n' + ">>> ");
+        resetColor();
     }
 
     public static void printError(String str) {
-        System.out.println(repeat("-", str.length()) + '\n' + str + '\n' + repeat("-", str.length()));
+        System.out.println(Color.RED.getColor() +
+                repeat("-", str.length() + 12) + '\n' + "[!]=[ " + str + " ]=[!]" + '\n' +
+                repeat("-", str.length() + 12));
+        resetColor();
     }
 
     public static void printColumn(String str) {
-        System.out.println(str);
+        System.out.println(Color.BLUE.getColor() + str);
+        resetColor();
+    }
+
+    private static void resetColor() {
+        System.out.print(Color.RESET.getColor());
     }
 }
