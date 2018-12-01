d8903338a7a800da352e280f829eb30ee302cd93
Breno Sousa <bress.io@outlook.com>
2018-12-01 18:19:13 -0200

    Adicionar a versão Legacy da Main

---
+++ src/com/bressio/LegacyMain.java
@@ -0,0 +1,23 @@
+package com.bressio;
+
+import java.util.Scanner;
+
+public class LegacyMain
+{
+    public static void main(String args[])
+    {
+        Scanner scanner = new Scanner(System.in);
+        int n;
+
+        System.out.println("Quantos simbolos?");
+        n = scanner.nextInt();
+
+        BinMinHeap heap = new BinMinHeap(n);
+        heap.carregaDados();
+        heap.imprime();
+        heap.aplicaHuffman();
+        heap.mostraCodigos();
+
+        scanner.close();
+    }
+}
